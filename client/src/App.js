0d1ccef2b7407c704594f60f746c0a96b7faf9da
Kenny Yang <kennyyangh@gmail.com>
2020-11-09 16:30:40 -0800

    starting react and header

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="App">
+      <Header />
+      {/* Tinder Cards */}
+      {/* Swipe Buttons */}
+    </div>
+  );
+}
+
+export default App;
